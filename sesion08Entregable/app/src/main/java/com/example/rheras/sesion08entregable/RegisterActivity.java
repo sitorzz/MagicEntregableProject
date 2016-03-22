5cdcde821581788cd8270c7e926a05e7c6c0c092
Albert7995 <amfernandez95@gmail.com>
2016-03-22 22:54:55 +0100

    Commit

--- sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/RegisterActivity.java
+++ sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/RegisterActivity.java
@@ -1,16 +1,27 @@
 package com.example.rheras.sesion08entregable;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     PlayerSQLiteHelper playerSQLiteHelper;
     SQLiteDatabase db;
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
+
+
 
 
     @Override
@@ -30,6 +41,7 @@ public class RegisterActivity extends AppCompatActivity {
         EditText editTextImage = (EditText) findViewById(R.id.editTextImage);
         EditText editTextLife = (EditText) findViewById(R.id.editTextlife);
 
+
         values.put("name", editTextName.getText().toString());
         values.put("image", editTextImage.getText().toString());
        values.put("life",editTextLife.getText().toString());
@@ -37,6 +49,53 @@ public class RegisterActivity extends AppCompatActivity {
 
         db.insert("Alumnos", null, values);
         db.close();
+    }
+
+    public void loadImagefromGallery(View view) {
+
+        // Create intent to Open Image applications like Gallery, Google Photos
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imgView);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 
 
