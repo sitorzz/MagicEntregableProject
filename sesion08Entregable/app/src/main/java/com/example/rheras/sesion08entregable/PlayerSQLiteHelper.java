8827452c4a010e86498ff5dcfbb4944420ac40a4
Albert7995 <amfernandez95@gmail.com>
2016-03-22 10:00:42 +0100

    PlayersSQLiteHelper hecho

---
+++ sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/PlayerSQLiteHelper.java
@@ -0,0 +1,29 @@
+package com.example.rheras.sesion08entregable;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by AlbertMartinez on 22/3/16.
+ */
+public class PlayerSQLiteHelper extends SQLiteOpenHelper {
+
+    String sqlCreate =  "CREATE TABLE players (id INTEGER, nombre TEXT)";
+
+    public PlayerSQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS players");
+        db.execSQL(sqlCreate);
+
+    }
+}
