2854c588e79502710b09ab82efb8615aa2d48bd4
Albert7995 <amfernandez95@gmail.com>
2016-04-03 21:51:48 +0200

    jifor

--- sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/RegisterActivity.java
+++ sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/RegisterActivity.java
@@ -55,28 +55,27 @@ public class RegisterActivity extends AppCompatActivity {
         String picturePath = "";
 
 
-        File internalStorage = playerSQLiteHelper.context.getDir("Pictures", Context.MODE_PRIVATE);
-        File playerFilePath = new File(internalStorage, editTextName.getText().toString() + ".png");
+    //    File internalStorage = playerSQLiteHelper.context.getDir("Pictures", Context.MODE_PRIVATE);
+     //  File playerFilePath = new File(internalStorage, editTextName.getText().toString() + ".png");
 
-        picturePath = playerFilePath.toString();
+      //  picturePath = playerFilePath.toString();
 
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(playerFilePath);
-            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100 /*quality*/, fos);
-            fos.close();
-        }
-        catch (Exception ex) {
-            Log.i("DATABASE", "Problem updating picture", ex);
-            picturePath = "";
-        }
+       // FileOutputStream fos = null;
+      //  try {
+     //       fos = new FileOutputStream(playerFilePath);
+     //       imageBitmap.compress(Bitmap.CompressFormat.PNG, 100 /*quality*/, fos);
+     //       fos.close();
+     //   }
+     //   catch (Exception ex) {
+     //       Log.i("DATABASE", "Problem updating picture", ex);
+     //   }
 
 
         values.put("name", editTextName.getText().toString());
 
         values.put("life",editTextLife.getText().toString());
 
-        values.put("image",picturePath);
+      //  values.put("image",picturePath);
 
 
         db.insert("players", null, values);
