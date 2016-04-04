c2619cf21d4414936516f7961e30fdf29563ff9b
Albert7995 <amfernandez95@gmail.com>
2016-04-04 18:30:25 +0200

    45

--- sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/RegisterActivity.java
+++ sesion08Entregable/app/src/main/java/com/example/rheras/sesion08entregable/RegisterActivity.java
@@ -46,7 +46,7 @@ public class RegisterActivity extends AppCompatActivity {
     public void registerPlayers(View view) {
 
         db = playerSQLiteHelper.getWritableDatabase();
-      //   img=Utilities.getBytes(BitmapFactory.decodeResource(getResources(), R.drawable.cute));
+      //  img=Utilities.getBytes(BitmapFactory.decodeResource(getResources(), R.drawable.));
 
         ContentValues values = new ContentValues();
         EditText editTextName = (EditText) findViewById(R.id.editTextName);
@@ -55,20 +55,20 @@ public class RegisterActivity extends AppCompatActivity {
         String picturePath = "";
 
 
-    //    File internalStorage = playerSQLiteHelper.context.getDir("Pictures", Context.MODE_PRIVATE);
-     //  File playerFilePath = new File(internalStorage, editTextName.getText().toString() + ".png");
+       File internalStorage = playerSQLiteHelper.context.getDir("Pictures", Context.MODE_PRIVATE);
+      File playerFilePath = new File(internalStorage, editTextName.getText().toString() + ".png");
 
-      //  picturePath = playerFilePath.toString();
+       picturePath = playerFilePath.toString();
 
-       // FileOutputStream fos = null;
-      //  try {
-     //       fos = new FileOutputStream(playerFilePath);
-     //       imageBitmap.compress(Bitmap.CompressFormat.PNG, 100 /*quality*/, fos);
-     //       fos.close();
-     //   }
-     //   catch (Exception ex) {
-     //       Log.i("DATABASE", "Problem updating picture", ex);
-     //   }
+        FileOutputStream fos = null;
+        try {
+           fos = new FileOutputStream(playerFilePath);
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100 /*quality*/, fos);
+           fos.close();
+       }
+       catch (Exception ex) {
+           Log.i("DATABASE", "Problem updating picture", ex);
+        }
 
 
         values.put("name", editTextName.getText().toString());
